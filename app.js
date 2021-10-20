d87c19c48a078eca502267efefb9c342783061b1
nitin <nitinmokashi08@gmail.com>
2021-10-20 16:00:37 +0530

    added get and post request handlers for root

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const https = require('https');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + "\\index.html");
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function(req, res) {
+    res.send('post is recived');
 });
 
 app.listen(3000);
