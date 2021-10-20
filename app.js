8540bfba26205f9dc6eb3d6258d4b7c58b7cf1fe
nitin <nitinmokashi08@gmail.com>
2021-10-20 16:11:57 +0530

    added a form index.html which will post city name and server will send weather data for that city

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/', function(req, res) {
-    res.send('post is recived');
+    const cityName = req.body.cityName;
+    const unit = 'metric';
+    const apiKey = 'b684cef43a43d1d1cd6c4bc59183f1e1'
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${unit}`;
+    https.get(url, function(responce) {
+        responce.on('data', function(data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDesc = weatherData.weather[0].description;
+            const icon = 'http://openweathermap.org/img/wn/' + weatherData.weather[0].icon + '@2x.png';
+            const city = weatherData.name;
+            res.write(`<p>Weather is currently ${weatherDesc} </p>`);
+            res.write(`<h1>The temperature in ${city} is ${temp} is degree Celcius.</h1>`);
+            res.write('<img style="background-color : grey;" src="' + icon + '">')
+            res.send();
+        });
+    });
 });
 
 app.listen(3000);
